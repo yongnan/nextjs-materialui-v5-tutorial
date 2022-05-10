a1bf040e89a30e52e550856ce5330696062a887a
yongnan <ync@fastmail.com>
2022-05-10 17:34:12 +0800

    u

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
+import EmotionButton from '../components/EmotionButton';
 
 const Home: NextPage = () => {
   return (
@@ -17,6 +18,7 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <EmotionButton />
       </main>
 
       <footer className={styles.footer}>
